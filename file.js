3c9f2459a1056317520b8f2cf3e12031944edecb
Esther3005 <esther.andria0409@gmail.com>
2023-06-28 14:59:41 +0300

    Projet blackpanther2

---
+++ file.js
@@ -0,0 +1,46 @@
+do {
+    newFunction();
+} while (condition)>
+<html>
+<head>
+  <title>Formulaire de Questions-Réponses</title>
+  <style>
+    h1 {
+      transition: scale(1);
+    &rbrace;
+ 
+    h1:hover {
+      transform: scale(1.2);
+    &rbrace;
+  </style>
+</head>
+<body>
+  <h1>ENIGME</h1>
+  <form>
+    <label for="ENIGME1">Prouve ton talent d’espion et déchiffre cette réplique d’Okoyé écrite en Wakandais, et
+apprend par la même occasion la première valeur d’une Dora Milaje !
+Le savais tu ? 
+
+
+Plusieurs sources s’entendent pour dire que la lettre la plus utilisée en français est la
+lettre E. En pourcentage de fréquence, la lettre est utilisée à 14% dans une phrase. Et
+si tu regardais quel symbole revient le plus souvent </label>
+    <input type="text" id="questiona" name="Alors, as tu la réponse ?"><br>
+    
+    <label for="ENIGME2">Une anecdote intéressante est écrite juste en dessous mais elle est codée par un code
+césar également appelé code de chiffrement par décalage . L’alphabet a été décalé,
+trouve la clé de chiffrement qui te permettrait de retrouver les lettres et retranscris la
+phrase :
+Tm nqtu lmjcbm i Wikstivl mv Kitqnwzvqm. Qt a'ioqb lm ti dqttm lwvb mab
+wzqoqviqzm Zgiv Kwwotmz mb moitmumvb ti dqttm ycq i dc viqbzm tm
+uwcdmumvb xwtqbqycm lma jtiks xivbpm</label>
+    <input type="text" id="" name="Alors, as tu la réponse ?"><br>
+    
+    <input type="submit" value="ENVOYER">
+  </form>
+
+  <script>
+    // Code JavaScript pour la pop-up
+    window.onload = function() {
+      var form = document.querySelector('form');
+      form.addEventListener('submit', function(event)&rbrace;
---
+++ index.js
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Défilement d'images en JavaScript</title>
+  <style>
+    #image-container {
+      width: 500px;
+      height: 300px;
+      overflow: hidden;
+    }
+    
+    #image-frame {
+      width: 1000px;
+      height: 300px;
+      display: flex;
+    }
+    
+    .image {
+      width: 500px;
+      height: 300px;
+    }
+  </style>
+</head>
+<body>
+  <div id="image-container">
+    <div id="image-frame">
+      <img class="image">
+        
+      <img class="image
+
